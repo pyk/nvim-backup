d851739f81cdc38343c4e8a44c9039963f3fdc7c
pyk <gm@pyk.sh>
2023-10-23 01:41:11 +0700

    chore: update git colorscheme

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,9 +1,32 @@
-vim.cmd.highlight({"LineNr", "ctermfg=240"})
-vim.cmd.highlight({"CursorLine", "cterm=none", "ctermbg=235"})
-vim.cmd.highlight({"CursorLineNr", "cterm=none", "ctermbg=235", "ctermfg=240"})
+-- Color table:
+-- https://upload.wikimedia.org/wikipedia/commons/1/15/Xterm_256color_chart.svg
+
+-- Basic
+vim.cmd([[
+  highlight LineNr ctermfg=240
+  highlight CursorLine cterm=none ctermbg=235
+  highlight CursorLineNr cterm=none ctermbg=235 ctermfg=white
+
+
+]])
+
+-- Git
+vim.cmd([[
+  highlight SignColumn ctermbg=none
+  highlight DiffAdd ctermbg=235 ctermfg=46
+  highlight DiffChange ctermbg=235 ctermfg=226
+  highlight DiffDelete ctermbg=235 ctermfg=196
+]])
 
 -- Markdown syntax highlighting
-vim.cmd.highlight({"Title", "ctermfg=46"})
-vim.cmd.highlight({"@spell.markdown", "ctermfg=40"})
-vim.cmd.highlight({"@text.literal.block.markdown", "ctermfg=47"})
-vim.cmd.highlight({"@conceal.markdown", "ctermfg=240"})
+vim.cmd([[
+  highlight @spell.markdown ctermfg=white
+  highlight Title                           cterm=bold    ctermfg=46
+  highlight @text.strong.markdown_inline    cterm=bold    ctermfg=40
+  highlight @text.emphasis.markdown_inline  cterm=italic  ctermfg=40
+  highlight @conceal.markdown_inline        ctermfg=46
+  highlight @punctuation.special.markdown   ctermfg=240
+  highlight @punctuation.delimiter.markdown   ctermfg=240
+  highlight @label.markdown   ctermfg=240
+  highlight @text.literal.block.markdown   ctermfg=47
+]])
